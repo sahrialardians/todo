51cf8abccf5df5c8a7265bb7a6ba9954c38777eb
Sahrial <sahrialardians@gmail.com>
2020-06-29 14:43:42 +0700

    Inisialisasi variable semua UI Element

---
+++ mini-project-1/starter/app.js
@@ -0,0 +1,6 @@
+// Kumpulkan semua UI Element
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const filterInput = document.querySelector('#filter-input');
+const todoList = document.querySelector('#todo-list');
+const clearButton = document.querySelector('#clear-todos');
