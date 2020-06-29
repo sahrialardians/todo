e1e2d793b9eed1d84388ff489ad8acfe088a1d9f
Sahrial <sahrialardians@gmail.com>
2020-06-29 15:14:12 +0700

    create function addTodo and DOM createElement

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -4,3 +4,33 @@ const todoInput = document.querySelector('#todo-input');
 const filterInput = document.querySelector('#filter-input');
 const todoList = document.querySelector('#todo-list');
 const clearButton = document.querySelector('#clear-todos');
+
+todoForm.addEventListener('submit', addTodo);
+
+function addTodo(e) {
+    e.preventDefault();
+
+    // Membuat li element
+    const li = document.createElement('li');
+
+    // menambahkan properti class pada element li
+    li.className = 'list-group-item d-flex justify-content-between align-items-center mb-1';
+    
+    // menambahkan childern kedalam element li
+    li.appendChild(document.createTextNode('Value dari input todo'));
+
+    // membuat delete button
+    const a = document.createElement('a');
+
+    // memberi properti untuk a element
+    a.href = '#';
+    a.className = 'badge badge-danger'
+
+    // memasukkan textnya ke child dengan cara ke 2
+    a.innerHTML = 'Delete';
+
+    // insert element a ke dalam childern li
+    li.appendChild(a);
+
+    console.log(li);
+}
