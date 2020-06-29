cd810eec031d252bbc000e076b9f52e92498bcc4
Sahrial <sahrialardians@gmail.com>
2020-06-29 20:32:23 +0700

    handle no empty input value

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -11,33 +11,37 @@ todoList.addEventListener('click', deleteTodo);
 function addTodo(e) {
     e.preventDefault();
 
-    // Membuat li element
-    const li = document.createElement('li');
+    if (todoInput.value) {
+        // Membuat li element
+        const li = document.createElement('li');
 
-    // menambahkan properti class pada element li
-    li.className = 'list-group-item d-flex justify-content-between align-items-center mb-1';
-    
-    // menambahkan childern kedalam element li
-    li.appendChild(document.createTextNode(todoInput.value));
+        // menambahkan properti class pada element li
+        li.className = 'list-group-item d-flex justify-content-between align-items-center mb-1';
 
-    // membuat delete button
-    const a = document.createElement('a');
+        // menambahkan childern kedalam element li
+        li.appendChild(document.createTextNode(todoInput.value));
 
-    // memberi properti untuk a element
-    a.href = '#';
-    a.className = 'badge badge-danger delete-todo'
+        // membuat delete button
+        const a = document.createElement('a');
 
-    // memasukkan textnya ke child dengan cara ke 2
-    a.innerHTML = 'Delete';
+        // memberi properti untuk a element
+        a.href = '#';
+        a.className = 'badge badge-danger delete-todo'
 
-    // insert element a ke dalam childern li
-    li.appendChild(a);
+        // memasukkan textnya ke child dengan cara ke 2
+        a.innerHTML = 'Delete';
 
-    // memasukkan element li yang telah dibuat js ke dalam element todo-list
-    todoList.appendChild(li);
+        // insert element a ke dalam childern li
+        li.appendChild(a);
 
-    // membuat reassigment nilai input jadi string kosong
-    todoInput.value = '';
+        // memasukkan element li yang telah dibuat js ke dalam element todo-list
+        todoList.appendChild(li);
+
+        // membuat reassigment nilai input jadi string kosong
+        todoInput.value = '';
+    } else {
+        alert("Field masih kosong, silahkan masukkan Todo!");
+    }
 }
 
 function deleteTodo(e) {
