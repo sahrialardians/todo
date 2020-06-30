b38fc52a29cc98b9507562939e3a922d18c37469
Sahrial <sahrialardians@gmail.com>
2020-06-30 10:49:07 +0700

    refactoring function addTodo & getTodos

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -25,9 +25,35 @@ function immediateLoadEventListener() {
     filterInput.addEventListener('keyup', filterTodos);
 }
 
-// DOM function
+// Reusable codes
+function createTodoElement(value) {
+    // Membuat li element
+    const li = document.createElement('li');
 
-function getTodos() {
+    // menambahkan properti class pada element li
+    li.className = 'todo-item list-group-item d-flex justify-content-between align-items-center mb-1';
+
+    // menambahkan childern kedalam element li
+    li.appendChild(document.createTextNode(value));
+
+    // membuat delete button
+    const a = document.createElement('a');
+
+    // memberi properti untuk a element
+    a.href = '#';
+    a.className = 'badge badge-danger delete-todo'
+
+    // memasukkan textnya ke child dengan cara ke 2
+    a.innerHTML = 'Delete';
+
+    // insert element a ke dalam childern li
+    li.appendChild(a);
+
+    // memasukkan element li yang telah dibuat js ke dalam element todo-list
+    todoList.appendChild(li);
+}
+
+function getItemsFromLocalStorage() {
     let todos;
 
     // mengecek kondisi local storage apakah gadak?
@@ -39,32 +65,19 @@ function getTodos() {
         todos = JSON.parse(localStorage.getItem('todos'));
     }
 
+    return todos;
+}
+
+// DOM function
+
+function getTodos() {
+    // inisialisasi variabel dari function getItemsfromlocalstorage
+    const todos = getItemsFromLocalStorage();
+
     // mengambil data todo dari localStorage
     todos.forEach((todo) => {
-        // Membuat li element
-        const li = document.createElement('li');
-
-        // menambahkan properti class pada element li
-        li.className = 'todo-item list-group-item d-flex justify-content-between align-items-center mb-1';
-
-        // menambahkan childern kedalam element li
-        li.appendChild(document.createTextNode(todo));
-
-        // membuat delete button
-        const a = document.createElement('a');
-
-        // memberi properti untuk a element
-        a.href = '#';
-        a.className = 'badge badge-danger delete-todo'
-
-        // memasukkan textnya ke child dengan cara ke 2
-        a.innerHTML = 'Delete';
-
-        // insert element a ke dalam childern li
-        li.appendChild(a);
-
-        // memasukkan element li yang telah dibuat js ke dalam element todo-list
-        todoList.appendChild(li);
+        // function untuk membuat element todo
+        createTodoElement(todo);
     });
 }
 
@@ -72,30 +85,8 @@ function addTodo(e) {
     e.preventDefault();
 
     if (todoInput.value) {
-        // Membuat li element
-        const li = document.createElement('li');
-
-        // menambahkan properti class pada element li
-        li.className = 'todo-item list-group-item d-flex justify-content-between align-items-center mb-1';
-
-        // menambahkan childern kedalam element li
-        li.appendChild(document.createTextNode(todoInput.value));
-
-        // membuat delete button
-        const a = document.createElement('a');
-
-        // memberi properti untuk a element
-        a.href = '#';
-        a.className = 'badge badge-danger delete-todo'
-
-        // memasukkan textnya ke child dengan cara ke 2
-        a.innerHTML = 'Delete';
-
-        // insert element a ke dalam childern li
-        li.appendChild(a);
-
-        // memasukkan element li yang telah dibuat js ke dalam element todo-list
-        todoList.appendChild(li);
+        // function untuk membuat element todo
+        createTodoElement(todoInput.value);
 
         // menambahkan todo ke dalam localStorage
         addTodoLocalStorage(todoInput.value);
@@ -107,17 +98,9 @@ function addTodo(e) {
     }
 }
 
-function addTodoLocalStorage(todoInputValue){
-    let todos;
-
-    // mengecek kondisi local storage apakah gadak?
-    if (localStorage.getItem('todos') == null) {
-        // jika key nya kosong, reassigment variabel todos jadi array kosong
-        todos = [];
-    } else {
-        // jika ada akan memasukkan value ke dalam variable todos atau reassigment lagi
-        todos = JSON.parse(localStorage.getItem('todos'));
-    }
+function addTodoLocalStorage(todoInputValue) {
+    // inisialisasi variabel dari function getItemsfromlocalstorage
+    const todos = getItemsFromLocalStorage();
 
     // mengpush value dari input todo ke dalam variable todos
     todos.push(todoInputValue);
